f4720ab4dee6565498875b7c74730161dd64799c
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2022-04-13 00:46:42 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Saif
+          Learn React with Saif ali
         </a>
       </header>
     </div>
