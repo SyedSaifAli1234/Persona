c71656258b6a2d80f2b277d293f60e86e962e8b5
Agha Muhammad Ali Nasir <anasir@i2cinc.com>
2022-04-15 10:40:16 +0500

    Layout

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,6 +1,23 @@
-const Signup = () => {
+const Signup=()=>{
     return (
-      <div>Signup</div>
+        <div>
+            <div className="form">
+                <form>
+                    <div className="input-container">
+                        <label>Username </label>
+                        <input type="text" name="uname" required />
+                    </div>
+                    <div className="input-container">
+                        <label>Password </label>
+                        <input type="password" name="pass" required />
+                    </div>
+                    <div className="button-container">
+                        <input type="submit" />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
+
 export default Signup;
